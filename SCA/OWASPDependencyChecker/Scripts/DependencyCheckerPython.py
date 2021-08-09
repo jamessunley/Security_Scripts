0dec5b271fb42cc4d6f010b10b2cdc7b7f5e821b
James Sunley <JamesSunley@Deltek.com>
2021-08-09 11:44:49 +0100

    Add detail to run tool, having Java install issues. will try again later

---
+++ SCA/OWASPDependencyChecker/Scripts/DependencyCheckerPython.py
@@ -0,0 +1,33 @@
+#Basic Python Script to teach myself Python.
+#Converted a script I already had from Powershell to Python
+#This Script runs OWASP DependencyChecker on a code base and outputs the results
+
+class DependencyCheck:
+
+    #imports
+    import os
+    #Variables
+
+    projectId = "ProjectName"
+    locationToScan = ""
+    #File path to supression file, this is not always needed to run Dependency Check
+    supressionFile = ""
+    outputLocation = ""
+    exclusionsList = ""
+    dependencyCheckLocation = "C:/Users/JamesSunley/Documents/dependency-check-6.2.2-release/dependency-check/bin/dependency-check.bat"
+
+    #Sets a list of folders and files to exclude
+    def Exclusions():
+        exclusions = "\Tests\**"
+        return exclusions
+
+    def Scan(locationToScan, supressionFile, outputLocation, exclusionsList, dependencyCheckLocation, os):
+            print("Starting Scan")
+            print("Folder to scan: " + locationToScan)
+            print("Report will be output to: " + outputLocation)
+            stream = os.popen(dependencyCheckLocation + ' --project `"projectId`" --scan `"locationToScan`" --suppression `"supressionFile`" exclusionsList --out `"outputLocation`" -f HTML')
+            output = stream.read()
+            print(output)
+
+    exclusionsList = Exclusions()
+    Scan(locationToScan, supressionFile, outputLocation, exclusionsList, dependencyCheckLocation, os)
